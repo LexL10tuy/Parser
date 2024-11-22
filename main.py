7a9ccc168aa54d4dbb81c3eff8b77813d173a147
LexL10tuy <aleshinalesha19011991@gmail.com>
2024-11-22 17:38:21 +0000

    Ok

---
+++ main.py
@@ -0,0 +1,12 @@
+import requests
+
+
+def main():
+    url = 'https://www.52shuku.vip/yanqing/hx4r_2828.html'
+    response = requests.get(url)
+    print(response.status_code)
+    with open('output', 'a', encoding='UTF-8') as file:
+        file.write(response.text)
+
+if __name__ == "__main__":
+    main()
