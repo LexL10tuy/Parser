a07d6b5227a9f55b7acf69909e919383a32e67fc
LexL10tuy <aleshinalesha19011991@gmail.com>
2024-11-29 18:25:21 +0000

    parser

--- main.py
+++ main.py
@@ -2,16 +2,23 @@ import requests
 from bs4 import BeautifulSoup
 
 def main():
-    url = 'https://tl.rulate.ru/book/80509/2660617/ready_new'
+    url = 'https://dikoed.ru/catalog/krokodil/'
     response = requests.get(url)
     print(response.status_code)
-
+    #print(response.text)
     soup = BeautifulSoup(response.text, 'html.parser')
-    part = soup.find('div', class_='content-text')
+    spoon = soup.find_all("div", class_='product-col list clearfix')
+    print([i.text for i in spoon])
+
+    with open('page.txt', 'a', encoding='utf-8') as file:
+        file.write(''.join([i.text for i in spoon]))
+
+    # soup = BeautifulSoup(response.text, 'html.parser')
+    # part = soup.find('div', class_='content-text')
 
 
-    with open('page.txt', 'a', encoding='UTF-8') as file:
-        file.write(part.text)
+    # with open('page.txt', 'a', encoding='UTF-8') as file:
+    #     file.write(part.text)
 
 if __name__ == "__main__":
     main()
